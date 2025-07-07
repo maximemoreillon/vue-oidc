328425feb938468d0eb4a5f66bd6c012f4cce34f
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-07 10:09:36 +0900

    oidc-auth v3.0.0, cicd

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -2,18 +2,30 @@ import type { App } from "vue";
 import useAuth from "../composables/useAuth";
 import OidcClient from "@moreillon/oidc-auth";
 
+export type Options = {
+  authority: string;
+  client_id: string;
+  extraQueryParams?: { [key: string]: string };
+  redirect_uri?: string;
+};
+
 let oidcClient: OidcClient;
 
 export default {
-  async install(app: App, options: any) {
+  async install(app: App, options: Options) {
     const auth = useAuth();
     oidcClient = new OidcClient(options);
     const oidcData = await oidcClient.init();
 
     if (!oidcData) return;
 
-    auth.user.value = oidcData.user;
-    auth.access_token.value = oidcData.access_token;
+    const {
+      user,
+      tokens: { access_token },
+    } = oidcData;
+
+    auth.user.value = user;
+    auth.access_token.value = access_token;
 
     oidcClient.onTokenRefreshed(({ access_token }) => {
       auth.access_token.value = access_token;
