2cb02c974d470892be8552d22d7ad249a147cf6e
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-07 11:13:39 +0900

    fixing login, logout

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -5,12 +5,22 @@ const client = ref<OidcClient | null>(null);
 const user = ref<any>(null);
 const access_token = ref<string | null>(null);
 
+function login() {
+  if (!client.value) throw new Error("OIDC Client not instanciated");
+  return client.value.login;
+}
+
+function logout() {
+  if (!client.value) throw new Error("OIDC Client not instanciated");
+  return client.value.logout;
+}
+
 export default function () {
   return {
     client,
     user,
     access_token,
-    login: client.value?.login,
-    logout: client.value?.logout,
+    login,
+    logout,
   };
 }
